c3537de8ecacf33106ac5e4244e59cc2222fda01
MichaelArango2023 <142031696+MichaelArango2023@users.noreply.github.com>
2023-08-11 09:55:32 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+
+
+/**
+ *
+ * @author Michael Arango
+ */
+//clase principal de java
+public class HolaMundo {
+ public static void main (String[] args){ 
+     System.out.println("Hola Michael");
+    }
+
+ }
+   
+
