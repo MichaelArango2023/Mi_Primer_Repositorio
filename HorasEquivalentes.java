df008b64461e3ab6803fcec1d88bd620dc9fc30a
MichaelArango2023 <142031696+MichaelArango2023@users.noreply.github.com>
2023-08-18 14:53:16 -0500

    Add files via upload

---
+++ HorasEquivalentes.java
@@ -0,0 +1,43 @@
+
+ *
+ * @author Michael Arango
+ */
+
+import javax.swing.JOptionPane;
+
+public class HorasEquivalentes {
+        public static void main(String[] args) {
+          //declarar las variables
+          
+          
+          int numero horas;
+          int semanas;
+          int dias;
+          
+          //declarar el numero total de horas, semanas, dias y horas equivalentes
+          
+          numero  horas = Integer.parseInt("numero de horas");
+          
+          numero  semanas = Integer.parseInt("numero de semanas");
+          
+          numero dias = Integer.parseInt("numero de dias");
+          
+          //proceso
+          
+          equivalencia = numero horas + numero semanas + numero dias;
+          
+          //muestro el resultado
+          
+          JOptionPane.showMessageDialog(null,"el numero de horas es: "+horas);
+          
+          
+                  
+                  
+                  
+           
+    }
+    /**
+     * @
+    }
+    
+
